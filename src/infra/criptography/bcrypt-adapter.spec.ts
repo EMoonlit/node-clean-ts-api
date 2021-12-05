fb225e37bdfc36a6e4348a1ee748e3ebfafc2ce2
Emerson Junqueira <emoonlit@gmail.com>
2021-12-05 15:38:41 -0300

    test: ensure BcryptAdapter throws if becrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -3,26 +3,46 @@ import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
-    return new Promise((resolve) => resolve('hash'))
+    return new Promise(resolve => resolve('hash'))
   }
 }))
 
-const salt = 12
-const makeSut = (): BcryptAdapter => {
-  return new BcryptAdapter(salt)
+interface SutTypes {
+  sut: BcryptAdapter
+  salt: number
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+  return {
+    sut,
+    salt
+  }
 }
 
 describe('Becrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const sut = makeSut()
+    const { sut, salt } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
-  });
+  })
 
   test('Should return a hash on success', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
-  });
-});
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const { sut } = makeSut()
+    const throwError = (): never => {
+      throw new Error()
+    }
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+
+  })
+})
