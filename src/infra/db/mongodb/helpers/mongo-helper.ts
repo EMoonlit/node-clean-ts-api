c0ac5fe4dd2ea7ceed160d1ccc78c234955e3181
Emerson Junqueira <emoonlit@gmail.com>
2021-12-11 17:56:56 -0300

    refactor: change map for any collection in MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
 import { Collection, MongoClient } from 'mongodb'
-import { AccountModel } from '../../../../domain/models/account'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -15,7 +14,7 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  map: (collection: any): AccountModel => {
+  map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, {
       id: _id.toString(),
