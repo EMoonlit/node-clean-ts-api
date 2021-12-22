722ac8f4c38a5b99d14a838ffb04931ac5f35bb9
Emerson Junqueira <emoonlit@gmail.com>
2021-12-22 15:25:04 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_passowrd'
+      })
+      .expect(200)
+  });
+});
