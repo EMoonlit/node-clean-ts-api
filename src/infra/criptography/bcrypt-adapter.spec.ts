70544dde593d20c7eed1dbfa4fd5bbb6b3709873
Emerson Junqueira <emoonlit@gmail.com>
2022-01-09 10:46:49 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -50,9 +50,15 @@ describe('Becrypt Adapter', () => {
   })
 
   test('Should call compare with correct values', async () => {
-    const { sut, salt } = makeSut()
+    const { sut } = makeSut()
     const compareSpy = jest.spyOn(bcrypt, 'compare')
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should return true when compare succeeds', async () => {
+    const { sut } = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
   })
 })
