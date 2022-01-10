7c1c331778a65b2feec6cb205ef5e77284936676
Emerson Junqueira <emoonlit@gmail.com>
2022-01-09 22:01:03 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password',
+        })
+        .expect(401)
+    })
   })
 })
